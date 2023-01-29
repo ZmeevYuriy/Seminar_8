196982e8f9513696820d94dee9830660efdf14b7
Yuriy_Zmeev <yuriy.zmeev@mail.ru>
2023-01-29 10:08:34 +0600

    add Task HW_8

---
+++ HW.py
@@ -0,0 +1,55 @@
+operators = {'+': (1, lambda x, y: x + y),'-': (1, lambda x, y: x - y),
+             '*': (2, lambda x, y: x * y),'/': (2, lambda x, y: x / y)}
+
+def eval_(formula):
+    def parse(formula_string):
+        number = ''
+        for s in formula_string:
+            if s in '1234567890.': # если символ - цифра, то собираем число
+                number += s
+            elif number: # если символ не цифра, то выдаём собранное число и начинаем собирать заново
+                yield float(number)
+                number = ''
+            if s in operators or s in "()": # если символ - оператор или скобка, то выдаём как есть
+                yield s
+        if number: # если в конце строки есть число, выдаём его
+            yield float(number)
+
+    def shunting_yard(parsed_formula):
+        stack = [] 
+        for token in parsed_formula:
+            # если элемент - оператор, то отправляем дальше все операторы из стека,
+            # чей приоритет больше или равен пришедшему,
+            # до открывающей скобки или опустошения стека.
+            if token in operators:
+                while stack and stack[-1] != "(" and operators[token][0] <= operators[stack[-1]][0]:
+                    yield stack.pop()
+                stack.append(token)
+            elif token == ")":
+                # если элемент - закрывающая скобка, выдаём все элементы из стека, до открывающей скобки,
+                # а открывающую скобку выкидываем из стека.
+                while stack:
+                    x = stack.pop()
+                    if x == "(":
+                        break
+                    yield x
+            elif token == "(":
+                stack.append(token)
+            else:
+                yield token
+        while stack:
+            yield stack.pop()
+
+    def calc(polish):
+        stack = []
+        for token in polish:
+            if token in operators: 
+                y, x = stack.pop(), stack.pop() 
+                stack.append(operators[token][1](x, y)) 
+            else:
+                stack.append(token)
+        return stack[0] 
+
+    print(calc(shunting_yard(parse(formula))))
+
+eval_(input('Ввод выражения:\n'))
